5123590587f200bb745b6a17d5818f38ab08e324
zee-18 <zeeakhter18@gmail.com>
2023-03-12 19:21:06 +0500

    deleted commit

--- app/main.js
+++ app/main.js
@@ -6,3 +6,5 @@ console.log('Changing commit message using -i rebase')
 
 console.log(' feature branch 2 work ......')
 console.log('Some new work in feature branch 1')
+
+console.log('Accidently invaluable work deleted practice..............')
