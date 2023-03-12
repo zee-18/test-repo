cee17d73b95c78c2c1469b45b9961da3aede3d1b
zee-18 <zeeakhter18@gmail.com>
2023-03-12 15:48:29 +0500

    new work in feature 1

--- app/main.js
+++ app/main.js
@@ -5,3 +5,4 @@ console.log('Interactive Rebase')
 console.log('Changing commit message using -i rebase')
 
 console.log(' feature branch 2 work ......')
+console.log('Some new work in feature branch 1')
